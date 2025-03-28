ffddf51ad2cfa9423bb3c984f07f84466154897e
AL3ARABIY <alarabiymahdi1980@gmail.com>
2025-03-28 16:28:29 +0000

    AuthorizationTestController: initialize only when local profile is active

--- src/main/java/com/al3arabiy/al3_backend/controllers/AuthorizationTestController.java
+++ src/main/java/com/al3arabiy/al3_backend/controllers/AuthorizationTestController.java
@@ -1,9 +1,11 @@
 package com.al3arabiy.al3_backend.controllers;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("local")
 @RestController @Deprecated
 @RequestMapping("/auth-test")
 public class AuthorizationTestController {
