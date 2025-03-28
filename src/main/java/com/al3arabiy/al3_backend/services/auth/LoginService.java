0e73495fa59ce26064ad7f5705a3055ba576690d
AL3ARABIY <alarabiymahdi1980@gmail.com>
2025-03-28 18:37:05 +0000

    LoginService: add @Validated

--- src/main/java/com/al3arabiy/al3_backend/services/auth/LoginService.java
+++ src/main/java/com/al3arabiy/al3_backend/services/auth/LoginService.java
@@ -12,10 +12,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
 
-@Service
+@Service @Validated
 @RequiredArgsConstructor
 public class LoginService {
 
