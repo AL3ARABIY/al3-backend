8247d64f54a532c4f50a817881d8d9a40f45cf1a
AL3ARABIY <alarabiymahdi1980@gmail.com>
2025-03-28 13:39:40 +0000

    create SecurityUserDetailsService

---
+++ src/main/java/com/al3arabiy/al3_backend/services/SecurityUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.al3arabiy.al3_backend.services;
+
+
+import com.al3arabiy.al3_backend.dto.CustomUserDetails;
+import com.al3arabiy.al3_backend.entities.User;
+import com.al3arabiy.al3_backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class SecurityUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new CustomUserDetails(user);
+    }
+}
