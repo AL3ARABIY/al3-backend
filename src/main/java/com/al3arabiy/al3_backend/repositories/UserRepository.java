cbf7bb526b8359af456c79ac5d47563286a6af37
AL3ARABIY <alarabiymahdi1980@gmail.com>
2025-03-28 14:03:46 +0000

    UserRepository: add @Repository

--- src/main/java/com/al3arabiy/al3_backend/repositories/UserRepository.java
+++ src/main/java/com/al3arabiy/al3_backend/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.al3arabiy.al3_backend.repositories;
 
 import com.al3arabiy.al3_backend.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
 }
