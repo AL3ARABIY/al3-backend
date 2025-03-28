5257cfea85c712f617cb4e24945cff5e284d63f0
AL3ARABIY <alarabiymahdi1980@gmail.com>
2025-03-28 13:39:30 +0000

    create CustomUserDetails

---
+++ src/main/java/com/al3arabiy/al3_backend/dto/CustomUserDetails.java
@@ -0,0 +1,43 @@
+package com.al3arabiy.al3_backend.dto;
+
+import com.al3arabiy.al3_backend.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (user.getRoles() == null) return new ArrayList<>();
+        return user.getRoles().stream().flatMap(role -> role.getPermissions().stream())
+                .map(permission -> new SimpleGrantedAuthority(permission.getName()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() { return user.getPassword(); }
+
+    @Override
+    public String getUsername() { return user.getUsername(); }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
