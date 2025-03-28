b433c89e7719425bf35960b79d1085acfd6e1260
AL3ARABIY <alarabiymahdi1980@gmail.com>
2025-03-28 14:03:22 +0000

    create RegistrationService

---
+++ src/main/java/com/al3arabiy/al3_backend/services/RegistrationService.java
@@ -0,0 +1,34 @@
+package com.al3arabiy.al3_backend.services;
+
+import com.al3arabiy.al3_backend.dto.RegistrationRequest;
+import com.al3arabiy.al3_backend.entities.User;
+import com.al3arabiy.al3_backend.repositories.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RegistrationService {
+
+    private final UserRepository userRepository;
+
+    public void register(@Valid RegistrationRequest registrationRequest) {
+
+        if(!registrationRequest.password().equals(registrationRequest.confirmPassword())) {
+            throw new IllegalArgumentException("Passwords do not match");
+        }
+
+        if(userRepository.findByUsername(registrationRequest.username()).isPresent()) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+
+        User user = new User();
+        user.setUsername(registrationRequest.username());
+        user.setPassword(registrationRequest.password());
+
+        userRepository.save(user);
+
+    }
+
+}
