fc5ecbbf980d08299e9ca3576d7d962d6b64b5cc
AL3ARABIY <alarabiymahdi1980@gmail.com>
2025-03-28 12:30:36 +0000

    intialize project

---
+++ src/main/java/com/al3arabiy/al3_backend/Al3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.al3arabiy.al3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Al3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Al3BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/al3arabiy/al3_backend/Al3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.al3arabiy.al3_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Al3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
